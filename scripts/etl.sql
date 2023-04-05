a191b55aa8ab1b5e4d1c98470d1b46e2c3865668
Zach Timmerman <zachary.timmerman@gmail.com>
2023-04-05 19:13:12 -0400

    Create etl script for loading CSV files into PostgreSQL

---
+++ scripts/etl.sql
@@ -0,0 +1,57 @@
+DROP DATABASE IF EXISTS products;
+CREATE DATABASE products;
+
+\c products;
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  slogan TEXT,
+  description TEXT,
+  category TEXT,
+  default_price TEXT
+);
+
+CREATE TABLE styles (
+  id SERIAL PRIMARY KEY,
+  product_id INT REFERENCES products(id),
+  name TEXT,
+  sale_price TEXT,
+  original_price VARCHAR(50),
+  default_style BOOLEAN
+);
+
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  value TEXT,
+  product_id INT REFERENCES products(id)
+);
+
+CREATE TABLE related_products (
+  id SERIAL PRIMARY KEY,
+  current_product_id INT REFERENCES products(id),
+  related_product_id INT
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  style_id INT REFERENCES styles(id),
+  url TEXT,
+  thumbnail_url TEXT
+);
+
+CREATE TABLE skus (
+  id SERIAL PRIMARY KEY,
+  style_id INT REFERENCES styles(id),
+  size TEXT,
+  quantity INT
+);
+
+COPY products(id, name, slogan, description, category, default_price) FROM '/users/zach/Documents/sdc-data/product.csv' DELIMITER ',' CSV HEADER;
+COPY styles(id, product_id, name, sale_price, original_price, default_style) FROM '/users/zach/Documents/sdc-data/styles.csv' DELIMITER ',' CSV HEADER;
+COPY related_products(id, current_product_id, related_product_id) FROM '/users/zach/Documents/sdc-data/related.csv' DELIMITER ',' CSV HEADER;
+COPY photos(id, style_id, url, thumbnail_url) FROM '/users/zach/Documents/sdc-data/photos.csv' DELIMITER ',' CSV HEADER;
+COPY skus(id, style_id, size, quantity) FROM '/users/zach/Documents/sdc-data/skus.csv' DELIMITER ',' CSV HEADER;
+
+SELECT * FROM skus;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
-const { Pool } = require('pq');
-
-const pool = new Pool(
-  host: process.env.DB_HOST
-);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use('/', routes)
 
 app.get('/', (req, res) => {
   res.send('hello world!');
